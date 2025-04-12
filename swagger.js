d860084664aaa9161b6a26cda14710dbcee7b906
jrgerodriguez <menjivar.jorge.93@gmail.com>
2025-04-12 07:54:19 -0600

    swagger done

--- swagger.js
+++ swagger.js
@@ -3,16 +3,34 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     title: 'Free Blog API',
-    description: 'This is documentation for our API'
+    description: 'This is documentation for our API',
   },
-  host: 'free-blog-project.onrender.com'
+  host: 'free-blog-project.onrender.com',
+  schemes: ['https'],
+  components: {
+    securitySchemes: {
+      oAuthNoScopes: {
+        type: 'oauth2',
+        flows: {
+          implicit: {
+            authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth',
+            scopes: {
+              'https://www.googleapis.com/auth/userinfo.email': 'Access to your email address',
+              'https://www.googleapis.com/auth/userinfo.profile': 'Access to your basic profile info',
+            },
+          },
+        },
+      },
+    },
+  },
+  security: [
+    {
+      oAuthNoScopes: ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile'],
+    },
+  ],
 };
 
 const outputFile = './swagger-output.json';
 const routes = ['./routes/index.js'];
 
-/* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
-root file where the route starts, such as index.js, app.js, routes.js, etc ... */
-
 swaggerAutogen(outputFile, routes, doc);
-
